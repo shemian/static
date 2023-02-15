82d6b4ac5181d7ed5ae9c64d42886c84f26cbf2b
shemian <shemian092@gmail.com>
2023-02-14 21:40:55 -0800

    Rendered the generated files to the base.html template

--- script.py
+++ script.py
@@ -2,6 +2,7 @@
 import os
 import markdown2
 import json
+from jinja2 import Environment, FileSystemLoader
 
 def read_config(config_file): # Reads the Configuration File and returns the dictionary 
     try:
@@ -17,6 +18,13 @@ def read_config(config_file): # Reads the Configuration File and returns the dic
 
 
 def convert_md_to_html(folder_path, image_path):
+
+    template_path = os.path.join(os.path.dirname(folder_path), 'templates')
+    loader = FileSystemLoader(template_path)
+    env = Environment(loader=loader)
+
+    base_template = env.get_template('base.html')
+
     for filename in os.listdir(folder_path): #loop through all the files as specified by `folder_path`
         if filename.endswith('.md'): # check if the file ends with an `.md` , then convert it to HTML
             file_path = os.path.join(folder_path, filename)
@@ -25,15 +33,20 @@ def convert_md_to_html(folder_path, image_path):
                     markdown_text = file.read() 
                     html = markdown2.markdown(markdown_text) #the markdown library converts the md file to HTML
                     html_filename = filename.replace('.md', '.html') #Replace the .md expension with the .html to get the html files
-                    html_file_path = os.path.join(folder_path, html_filename) # Constructing the full HTML file path by joining the `folder_path` and `html_filename`
+                    html_file_path = os.path.join(template_path, html_filename) # Constructing the full HTML file path by joining the `folder_path` and `html_filename`
                     #create a list of image paths
                     image_list = []
                     for file in os.listdir(image_path):
                         if file.endswith(('.png', '.jpg', '.jpeg')):
                             image_list.append(os.path.join(image_path, file))
 
-                    with open(html_file_path, 'w') as html_file: #open the HTMl file in write Mode
-                        html_file.write(html.format(image_list=image_list))
+
+
+                     # render the template using jinja2
+                    context = {'content': html, 'image_list': image_list}
+                    final_html = base_template.render(context)
+                    with open(html_file_path, 'w',encoding='utf-8') as html_file: #open the HTMl file in write Mode
+                        html_file.write(final_html)
                         print(f'Converted {filename} to {html_filename}')
 
             except FileNotFoundError:
@@ -50,7 +63,7 @@ def main():
         image_path = config['image_path']
         
         if os.path.isdir(folder_path):
-            convert_md_to_html(folder_path, image_path)
+            convert_md_to_html(folder_path, image_path,)
 
         else:
             print(f'Error: Folder {folder_path} not found.')
